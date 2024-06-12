dabb9176121035cf1857ac9676d61742791d6016
NONE <NONE>
2024-06-12 14:16:46 -0500

    Initial Commit

---
+++ ObjectiveCLibrary/ObjectiveCLibrary.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveCLibrary.h
+//  ObjectiveCLibrary
+//
+//  Created by Work on 12/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveCLibrary.
+FOUNDATION_EXPORT double ObjectiveCLibraryVersionNumber;
+
+//! Project version string for ObjectiveCLibrary.
+FOUNDATION_EXPORT const unsigned char ObjectiveCLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveCLibrary/PublicHeader.h>
+
+
