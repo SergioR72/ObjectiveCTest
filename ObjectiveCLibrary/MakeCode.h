962fdf0dfcad07602915949a91e10c12a60084c9
NONE <NONE>
2024-06-12 14:39:16 -0500

    first commit

---
+++ ObjectiveCLibrary/MakeCode.h
@@ -0,0 +1,18 @@
+//
+//  MakeCode.h
+//  ObjectiveCLibrary
+//
+//  Created by Work on 12/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MakeCode : NSObject
+
+- (NSString *)giveCode;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjectiveCLibrary/MakeCode.m
@@ -0,0 +1,16 @@
+//
+//  MakeCode.m
+//  ObjectiveCLibrary
+//
+//  Created by Work on 12/06/24.
+//
+
+#import "MakeCode.h"
+
+@implementation MakeCode
+
+- (NSString *)giveCode {
+    return @"Hola mundo from objective c interop";
+}
+
+@end
